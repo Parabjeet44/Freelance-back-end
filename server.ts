b9d72006c0957aac6c339225000e02ca75d381da
Anova44 <gurdeepprajpat@gmail.com>
2026-01-08 23:48:08 +0530

    Updated server

--- server.ts
+++ server.ts
@@ -13,6 +13,8 @@ const app=express();
 app.use(cors({
     origin:process.env.FRONT_END_URL,
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(cookieParser());
 app.use(express.json());
